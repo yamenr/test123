d785250216c9a97edae50b6873fdebce4f6ab558
yamenr <yamen.rock@gmail.com>
2023-09-22 10:04:01 +0300

    add new login

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 // klsjefjsdfksjdfkdjfksjfksjdfksdj
+    //s;fdhkl;fh;ldfhl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
